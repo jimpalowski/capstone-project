d053f414dab944074f15c88bf43dc97dc0449830
Jim Palowski <palowskijim@gmail.com>
2018-07-20 16:49:44 -0700

    Added Redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+
+
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom'
+import store from './config/store'
 
-const app = <BrowserRouter>
-  <App />
-</BrowserRouter>
+
+const app = <Provider store={store}>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+</Provider>
+
 
 ReactDOM.render(app, document.getElementById('root'));
