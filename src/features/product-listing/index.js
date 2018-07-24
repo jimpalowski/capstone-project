0bdab953baec485028c36ccb2fc3b671353636b9
Jim Palowski <palowskijim@gmail.com>
2018-07-24 16:34:30 -0700

    Add state reducer

--- src/features/product-listing/index.js
+++ src/features/product-listing/index.js
@@ -1,9 +1,8 @@
 import React from 'react'
-
 import ProductListItem from './product-list-item'
+import { connect } from 'react-redux'
 
-
-export default function ProductListing(props){
+function ProductListing(props){
     return (<div className='product-listing'>
         {
             props.products.map( product =>
@@ -12,3 +11,24 @@ export default function ProductListing(props){
         </div>
       )
 }
+
+function mapStateToProps(state) {
+  return {
+    cart: state.cart
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    addToCart: (item) => {
+      dispatch({ type: 'ADD',
+        payload: item})
+    },
+    removeFromCart: (item) => {
+      dispatch({ type: 'REMOVE',
+                  payload: item})
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProductListing)
