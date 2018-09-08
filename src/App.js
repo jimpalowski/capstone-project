1b2ea24191592e2ca027c4616c8f55aa0428083c
Jim Palowski <palowskijim@gmail.com>
2018-09-07 23:56:32 -0700

    Add products link

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ const Navigation = (props) =>
     <ul>
       <li><NavLink to='/'>Home</NavLink></li>
       <li><NavLink to='/cart'>Cart</NavLink></li>
+      <li><NavLink to='/products'>Products</NavLink></li>
     </ul>
   </nav>
 
