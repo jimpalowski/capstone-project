81b435eb9cf027d863dc54c2326a381f1d642949
Jim Palowski <palowskijim@gmail.com>
2018-10-06 22:52:16 -0700

    Removed index.css

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 
-import './index.css';
 import App from './App';
 import store from './config/store'
 import ProductListItem from './features/product-listing/product-list-item';
